6c9e18eb2a77467b3a42f8efecde8246db10c4b5
Pradeep Kolli <pradeepkolli3@yahoo.in>
2020-04-09 18:21:46 -0500

    pushing data to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const axios = require("axios");
+  let data = [];
+axios({
+    "method":"GET",
+    "url":"https://covid-193.p.rapidapi.com/statistics",
+    "headers":{
+    "content-type":"application/octet-stream",
+    "x-rapidapi-host":"covid-193.p.rapidapi.com",
+    "x-rapidapi-key":"554a0664a6mshd03eb0ca9f9d535p14f556jsn71a88dc87bed"
+    }
+    })
+    .then((response)=>{
+      console.log(response.data.response)
+      data = response.data.response
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+    const items = data.map(item => 
+      {
+        return <div>{item}</div>
+      })
+    console.log(items)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <code>
+          data
+          {items}
+        </code>
       </header>
     </div>
   );
 }
-
 export default App;
